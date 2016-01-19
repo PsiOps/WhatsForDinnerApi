33ce63b2f0db75a8ea22d3685680b11135ef9d78
psiops <paul.siersma@gmail.com>
2016-01-19 20:47:35 +0000

    Some changes to server, added todos

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 
+// TODO: require a config.js (gitignore) which stores db settings 
+// TODO: Add swagger stuff to this api
+// TODO: Find out how to add authentication to this api
+// TODO: Move routes to their own files, like in swagger generated server code
+
 // Models
 var Recipe = require("./app/models/recipe")
 
@@ -15,10 +20,17 @@ app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;        // set our port
 
+var router = express.Router();              // get an instance of the express Router
+
+// middleware to use for all requests
+router.use(function(req, res, next) {
+    // do logging
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
+});
+
 // all of our routes will be prefixed with /api
 app.use('/api', router);
-
-var router = express.Router();              // get an instance of the express Router
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
@@ -26,11 +38,30 @@ router.get('/', function(req, res) {
 });
 
 router.route("/recipes")
-    .post(function(res, req){
+    .post(function(req, res){
         
-        var recipe = new 
+        var recipe = new Recipe();
+        
+        recipe.name = req.body.name;
+        
+        recipe.save(function(err){
+            if(err){
+                res.send(err);
+            }
+                
+            res.json({message: "Recipe created!"})
+        })
     });
 
+    .get(function(req, res) {
+        Recipe.find(function(err, recipes) {
+            if (err){
+                res.send(err);
+            }
+
+            res.json(recipes);
+        });
+    });
 
 app.listen(port);
 console.log('Magic happens on port ' + port);
