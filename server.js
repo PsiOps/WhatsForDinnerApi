b1e95bb293fadc8acd92339eb907af8343ed0d59
psiops <paul.siersma@gmail.com>
2016-01-20 19:51:52 +0000

    Some prettifying

--- server.js
+++ server.js
@@ -1,29 +1,32 @@
 var express = require("express");
-var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 
+// Start the app
+var app = express();
+
+// Load configuration
 var config = require("./config.js")
 
 // TODO: Implement token based authentication
 // TODO: Add swagger stuff to this api
 
-// Models
-var Recipe = require("./app/models/recipe")
-
+// Connect to db
 mongoose.connect(config.db.host + ":27017"); 
 
-// configure app to use bodyParser()
-// this will let us get the data from a POST
+// configure app to use bodyParser. This will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080;        // set our port
+// set our port
+var port = process.env.PORT || 8080;
 
+// Add routes
 var router = require("./app/routes/routes.js")
 
 // all of our routes will be prefixed with /api
 app.use('/api', router);
 
+// Start listening
 app.listen(port);
 console.log('Magic happens on port ' + port);
